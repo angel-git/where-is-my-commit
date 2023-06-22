afe45f5e46415d19d27e1c0dec0fe4ebe544e6dd
agavalda <agavalda@digital.ai>
2023-06-22 14:48:14 +0200

    update v0.3.4

--- gw.v
+++ gw.v
@@ -15,7 +15,7 @@ fn main() {
 	mut cmd := cli.Command{
 		name: 'gw'
 		description: 'Where is my commit?\nUsage:\ngw search -help\ngw diff -help'
-		version: '1.3.0'
+		version: '1.3.1'
 	}
 	mut search_cmd := cli.Command{
 		name: 'search'
